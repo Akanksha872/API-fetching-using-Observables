aba105879f7de064c1cb211a70696cd6d2a98df5
Akanksha872 <akankshasingh872@gmail.com>
2022-04-15 16:23:33 +0530

    added switchMap

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
-import { fromEvent, of } from 'rxjs';
+import { fromEvent, merge, NEVER, of, timer } from 'rxjs';
 import { fromFetch } from 'rxjs/fetch';
-import { catchError, exhaustMap, mergeMap, retry, tap } from 'rxjs/operators';
+import {
+  catchError,
+  exhaustMap,
+  mapTo,
+  mergeMap,
+  retry,
+  switchMap,
+  tap,
+} from 'rxjs/operators';
 
 import { createElement } from './helper';
 
@@ -8,6 +16,9 @@ const errorStatus = document.getElementById('error');
 const fetchButton = document.getElementById('get-dog-facts');
 const stopButton = document.getElementById('stop-dog-facts');
 const factsSection = document.getElementById('dog-facts');
+
+const fetch$ = fromEvent(fetchButton, 'click').pipe(mapTo(true));
+const stop$ = fromEvent(stopButton, 'click').pipe(mapTo(false));
 const clearFacts = () => {
   factsSection.innerText = '';
 };
@@ -23,34 +34,39 @@ const addFact = ({ fact }) => {
 };
 const addFacts = (data) => data.facts.forEach(addFact);
 
-const endpoint =
-  'https://rxjs-api.glitch.me/api/facts?delay=2000&chaos=true&flakiness=2';
+const endpoint = 'https://rxjs-api.glitch.me/api/facts';
 
-const fetch$ = fromEvent(fetchButton, 'click').pipe(
-  exhaustMap(() => {
-    return fromFetch(endpoint).pipe(
-      tap(clearError),
-      mergeMap((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Oops!!!Something went wrong');
-        }
-      }),
-      retry(4),
-      catchError((error) => {
-        clearFacts();
-        return of({ error: error.message });
-      })
-    );
+const fetchData = () => {
+  return fromFetch(endpoint).pipe(
+    tap(clearError),
+    mergeMap((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Oops!!!Something went wrong');
+      }
+    }),
+    retry(4),
+    catchError((error) => {
+      console.error(error);
+      clearFacts();
+      return of({ error: error.message });
+    })
+  );
+};
+
+const factStream$ = merge(fetch$, stop$).pipe(
+  switchMap((shouldFetch) => {
+    if (shouldFetch) {
+      return timer(0, 500).pipe(
+        tap(() => clearError()),
+        tap(() => clearFacts()),
+        exhaustMap(fetchData)
+      );
+    } else {
+      return NEVER;
+    }
   })
 );
 
-fetch$.subscribe(({ facts, error }) => {
-  if (error) {
-    return setError(error);
-  } else {
-    clearFacts();
-    addFacts({ facts });
-  }
-});
+factStream$.subscribe(addFacts);
